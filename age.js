7c7c02150ae1dd6d10fb5849ea466c39c511fdfc
daniemeas <danie.meas@gmail.com>
2019-11-20 20:32:09 -0600

    add JS functionality finished

--- age.js
+++ age.js
@@ -1,1 +1,16 @@
-console.log('hi');
+//Challenge 1: Your age in days
+
+function ageInDays() {
+  var birthYear = prompt('What year were you born in?');
+  var ageinDayss =(2019-birthYear)*365;
+  var h1 = document.createElement('h1');
+  var textAnswer = document.createTextNode('You are ' + ageinDayss + ' days old');
+  h1.setAttribute('id', 'ageInDays');
+  h1.appendChild(textAnswer);
+  document.getElementById('flex-box-result').appendChild(h1);
+
+}
+function reset() {
+  document.getElementById('ageInDays').remove();
+}
+
