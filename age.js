e17c34658e55eb60b6fd91736ba875b698c404b2
daniemeas <danie.meas@gmail.com>
2019-11-25 20:47:29 -0600

    rps win and lose functions

--- age.js
+++ age.js
@@ -42,3 +42,11 @@ function randToRpsInt() {
 function numberToChoice(number) {
   return ['rock', 'paper', 'scissors'][number];
 }
+function decideWinner(yourChoice, computerChoice) {
+  var rpsDataBase = {
+    'rock': {'scissors':1, 'rock': 0.5, 'paper': 0},
+    'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+    'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0},
+  }
+
+}
