517963c45176a52404f439eb54847a64a69bec4d
daniemeas <danie.meas@gmail.com>
2019-11-26 20:20:51 -0600

    more js

--- age.js
+++ age.js
@@ -27,10 +27,11 @@ function generateCat() {
 function rpsGame(yourChoice) {
   console.log(yourChoice);
   var humanChoice, botChoice;
-  //humanChoice = yourChoice.id;
+  humanChoice = yourChoice.id;
   botChoice = numberToChoice(randToRpsInt());
-  console.log(botChoice);
-  //results = decideWinner(humanChoice, botChoice); //may return an array.
+  console.log('computerChoice:',botChoice);
+  results = decideWinner(humanChoice, botChoice); //may return an array.
+  console.log(results);
   //message = finalMessage(results); //{message:"you won", 'color': 'green'} an object.
   //rpsFrontEnd(yourChoice.id, botChoice, message);
 }
@@ -43,10 +44,15 @@ function numberToChoice(number) {
   return ['rock', 'paper', 'scissors'][number];
 }
 function decideWinner(yourChoice, computerChoice) {
-  var rpsDataBase = {
-    'rock': {'scissors':1, 'rock': 0.5, 'paper': 0},
+  var rpsDatabase = {
+    'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
     'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
-    'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0},
-  }
+    'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
+  };
 
+  var yourScore = rpsDatabase[yourChoice][computerChoice];
+  var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+  return [yourScore, computerScore];
 }
+console.log()
