03e1cd8caa8af4b01a18d6605fc901c8978db185
daniemeas <danie.meas@gmail.com>
2019-12-05 21:05:39 -0600

    function behind the buttons

--- age.js
+++ age.js
@@ -103,7 +103,26 @@ console.log(all_buttons);
 
 var copyAllButtons = [];
 for (let i=0; i < all_buttons.length; i ++) {
-  copyAllButtons.push(all_buttons[i]);
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+console.log(copyAllButtons)
+
+
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value === 'red') {
+    buttonsRed();
+  } else if (buttonThingy.value === 'green') {
+    buttonsGreen();
+  } else if (buttonThingy.value === 'reset') {
+    buttonColorReset();
+  } else if (buttonThingy.value === 'random') {
+    randomColors();
+  }
 }
 
-console.log(copyAllButtons);
+function buttonsRed() {
+  for(let i=0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+
+  }
+}
