4d776b1250c58310bb0ab908e4797fbc0adfcb58
daniemeas <danie.meas@gmail.com>
2019-12-21 10:12:23 -0600

    add random cards to show

--- age.js
+++ age.js
@@ -154,7 +154,8 @@ function randomColors() {
 
 let blackjackGame = {
   'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
-  'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0}
+  'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+  'cards': ['2', '3', '4','5','6','7','8','9','10','K','J','Q','A'],
 };
 
 const YOU = blackjackGame['you']
@@ -166,14 +167,16 @@ document.querySelector('#blackjack-hit-button').addEventListener('click', blackj
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
-  showCard(YOU);
-  showCard(DEALER);
+  let card = randomCard();
+  console.log(card);
+  showCard(card, YOU);
+  
   
 }
 
-function showCard(activePlayer) {
+function showCard(card, activePlayer) {
 let cardImage = document.createElement('img');
-cardImage.src = 'images/Q.png';
+cardImage.src = `images/${card}.png`;
 document.querySelector(activePlayer['div']).appendChild(cardImage);
 hitSound.play();
 }
@@ -189,4 +192,9 @@ function blackjackDeal() {
     dealerImages[i].remove();
 
   }
+}
+
+function randomCard() {
+  let randomIndex = Math.floor(Math.random() * 13);
+  return blackjackGame['cards'][randomIndex];
 }
