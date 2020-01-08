6851821e38d1bc82bb8919fe56a2a169f95ec214
daniemeas <danie.meas@gmail.com>
2020-01-07 20:39:47 -0600

    remove score once player busts

--- age.js
+++ age.js
@@ -199,6 +199,12 @@ function blackjackDeal() {
   }
   YOU['score'] = 0;
   DEALER['score'] = 0;
+
+  document.querySelector('#your-blackjack-result').textContent = 0;
+  document.querySelector('#dealer-blackjack-result').textContent = 0;
+
+  document.querySelector('#your-blackjack-result').style.color = '#ffffff';
+  document.querySelector('#dealer-blackjack-result').style.color = '#ffffff';
 }
 
 function randomCard() {
