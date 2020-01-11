15a56bdf2ee24f593b38e67c2b340b7452dd1650
daniemeas <danie.meas@gmail.com>
2020-01-11 11:26:57 -0600

    add wins losses and draws to table

--- age.js
+++ age.js
@@ -156,7 +156,11 @@ let blackjackGame = {
   'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
   'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
   'cards': ['2', '3', '4','5','6','7','8','9','10','K','J','Q','A'],
-  'cardsMap' :{'2': 2, '3': 3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'K':10, 'J':10, 'Q':10, 'A':[1,11] }
+  'cardsMap' :{'2': 2, '3': 3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'K':10, 'J':10, 'Q':10, 'A':[1,11]}, 
+  'wins': 0, 
+  'losses': 0,
+  'draws': 0,
+  
 };
 
 const YOU = blackjackGame['you']
@@ -244,55 +248,63 @@ function dealerLogic() {
   showCard(card, DEALER);
   updateScore(card, DEALER);
   showScore(DEALER);
-  showResult(computeWinner());
+  
+
+  if (DEALER['score'] > 15) {
+    let winner = computeWinner();
+    showResult(winner);
+  }
 
 }
 
 //compute winner and return who just won
+//update the wins, draws, and losses
 function computeWinner() {
   let winner;
 
   if (YOU['score'] <= 21) {
     //condition: higher score than delaer or when dealer busts you're 21 under.
     if(YOU['score'] > DEALER['score'] ||(DEALER['score'] > 21)) {
-      console.log('YOU WON!');
+      blackjackGame['wins']++;
       winner = YOU;
 
     } else if (YOU['score'] < DEALER['score']) {
-      console.log('YOU LOST!');
+      blackjackGame['losses']++;
       winner = DEALER;
 
     } else if (YOU['score'] === DEALER['score']) {
-      console.log('YOU DREW!');
+      blackjackGame['draws']++;
     }
   //condition: when user busts but dealer doesn't
   } else if (YOU['score'] > 21 && DEALER['score'] <= 21) {
-    console.log('YOU LOST');
     winner = DEALER;
 
     //condition: when you and the dealer bust:
   } else if(YOU['score'] > 21 && DEALER['score'] > 21) {
-    console.log('YOU DREW!');
+    
   }
 
 
-  console.log('Winner is', winner);
+  console.log(blackjackGame);
   return winner;
 }
 function showResult(winner) {
   let message, messageColor;
 
   if (winner === YOU) {
+    document.querySelector('#wins').textContent = blackjackGame['wins'];
     message = 'You Won!';
     messageColor = 'green';
     windSound.play();
 
   } else if (winner === DEALER){
+    document.querySelector('#losses').textContent = blackjackGame['losses'];
     message = 'You lost!';
     messageColor = 'red';
     lossSound.play();
 
   } else {
+    document.querySelector('#draws').textContent = blackjackGame['draws'];
     message = 'You drew!';
     messageColor = 'black';
   }
